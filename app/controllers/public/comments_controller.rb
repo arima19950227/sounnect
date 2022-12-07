abc09a1619410264c0c4870d675768836f57ba7e
arima19950227 <arima.macbook@gmail.com>
2022-12-07 08:41:38 +0000

    コメント機能実装

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,9 +1,23 @@
 class Public::CommentsController < ApplicationController
 
  def create
+   @review = Review.find(params[:review_id])
+   comment = current_user.comments.new(comment_params)
+   comment.review_id = @review.id
+   comment.save
+   redirect_to request.referer
  end
- 
+
  def destroy
-   
+  @review = Review.find(params[:review_id])
+  Comment.find(params[:id]).destroy
+  redirect_to request.referer
+ end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
 
 end
